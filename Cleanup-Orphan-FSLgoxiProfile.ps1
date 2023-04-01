d3214404bea2cad357eca1ef641c39747b790222
Siva Shanker <47060139+SivaShanker21@users.noreply.github.com>
2023-04-01 19:27:17 +0800

    Add files via upload

---
+++ Cleanup-Orphan-FSLgoxiProfile.ps1
@@ -0,0 +1,48 @@
+﻿<#
+     .REQUIREMENTS
+        Read and write access to the SMB share
+        AD Powershell Module
+     .SYNOPSIS
+        Cleanup orphan FsLogix profile
+    .DESCRIPTION
+        Retrieves all orphan AVD Fslogix profile and delete it
+    .NOTES
+        Author: Siva Shanker Balakrishnan
+    .INPUTS
+        You will need to enter the SMB location
+    .DISCLAIMER
+        Please test the script in QC before using it in PROD.
+
+#>
+
+$loc= "us"
+$stgnetworkpath = "\\servernameFQDN\userprofileshare"
+
+$allProfileFolder = Get-ChildItem -Path $stgnetworkpath
+[array]$usernames=@()
+[array]$notinad=@()
+$currentdate = Get-Date -Format ddMMyyyy
+foreach($profileFolder in $allProfileFolder ){
+
+   $username = $profileFolder.Name
+   $remove = $username.IndexOf("_")
+   $adname = $username.Substring(0,$remove)
+
+   $testuser = try{
+    Get-ADUser -Filter 'SamAccountName -eq $adname'
+   }catch{
+
+      $null
+
+   }
+
+   if($testuser -eq $null){
+        $notinad += $adname
+        Remove-Item -Path "$stgnetworkpath\$profileFolder" -Force -Recurse
+
+   }
+}
+
+$notinad | Out-File -FilePath "C:\temp\$($loc)_removed_UserProfile_$($currentdate).txt" -Force
+
+Write-Host "Total orphan found:"$notinad.Count -ForegroundColor Red
---
+++ InactiveFSLogixProfiles.ps1
@@ -0,0 +1,46 @@
+﻿<#
+     .REQUIREMENTS
+        Read and write access to the SMB share
+     .SYNOPSIS
+        Gets or Gets and Delete inavtive FsLogix profile  
+    .DESCRIPTION
+        Retrieves all AVD Fslogix inactive profile past X days
+    .NOTES
+        Author: Siva Shanker Balakrishnan
+    .INPUTS
+        You will need to enter the SMB location
+    .DISCLAIMER
+        Please test the script in QC before using it in PROD.
+
+#>
+
+
+$loc= "us" # set storage location for report/export result if you have more than one storage location
+$stgnetworkpath = "\\servernameFQDN\userprofileshare" # Fslogix user profile SMB path
+$date = Get-Date -Format 'ddMMyyyy'
+$Days = "30" # Number of days before current date
+#Calculate Cutoff date
+$CutoffDate = (Get-Date).AddDays(-$Days)
+
+$allProfileFolder = Get-ChildItem -Path $stgnetworkpath
+[array]$vhds = @()
+
+foreach($profileFolder in $allProfileFolder){
+    
+   $vhd = Get-ChildItem -Path "$($stgnetworkpath)\$($profileFolder)" -Filter 'Profile*' 
+    
+    If($vhd.LastWriteTime -lt $CutoffDate) {
+        
+        $username = $profileFolder.Name
+        $remove = $username.IndexOf("_")
+        $adname = $username.Substring(0,$remove)
+
+        $vhds += $vhd
+        Write-Host "Removing $($adname)" -ForegroundColor White -BackgroundColor Red
+        #Remove-Item -Path "$($stgnetworkpath)\$($profileFolder)" -Force -Recurse #Remove the comment block if you want to delete inactive user profile
+    }
+
+}
+
+$vhds | select Name,LastWriteTime,LastAccessTime | Sort-Object LastWriteTime | Export-Csv -Path "C:\temp\$($loc)_fslogix_profilelastwrite_$($date).csv" -NoTypeInformation
+$vhds.Count
